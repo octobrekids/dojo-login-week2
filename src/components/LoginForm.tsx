eec3d8a126264e38ab0d9bff09b9ea6033f44a1c
octobrekids <chnrd.k@gmail.com>
2020-09-25 04:03:05 +0700

    Change to dynamic form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,26 +1,26 @@
 
 import React from 'react'
-import { FormValue } from '../hooks/useLogin'
+import { FormState } from '../model/interface'
 
-const LoginForm: React.FC<{
-    values: FormValue[],
-    errors: boolean,
-    loading: boolean,
-    loggedIn: boolean,
-    onChange:(e: React.ChangeEvent<HTMLInputElement>) => void,
-    onSubmit:() => void,
-    }> = (props) => {
 
-        const {errors,loading,loggedIn,onChange,onSubmit} = props
+
+const LoginForm: React.FC<FormState> = (props) => {
+
+        const {values,errors,loading,loggedIn,onChange,onSubmit} = props
     
     return (
         <div>
-            <label>Username</label><br />
-            <input type="text" id="username" name="username" onChange={onChange}/><br />
-            <label>Password</label><br />
-            <input type="text" id="password" name="password" onChange={onChange}/><br /><br />
+            {values && values.map((e, i) =>
+                <div>
+                    {e.name}<br />
+                    <label key={i}>
+                        <input value={e.value} name={e.name} onChange={onChange} />
+                    </label>
+                    <br />
+                </div>
+            )}
             {errors ? <p>please check username or password</p> : null}
-            {loading ? <div className="loader"></div> : null}
+            {loading ? <p>loading...</p> : null}
             {loggedIn ? <p>Login success</p> : null}
             <button type="submit" onClick={onSubmit}>Login</button>
         </div>
